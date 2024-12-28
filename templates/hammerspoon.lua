d42b6642335b6137ff32bbf52669626a59285302
Aleksandar Goševski <gosevskibg@gmail.com>
2024-12-28 14:45:57 +0100

    feat: add toggle for terminal auto-hide and support multiple terminals

--- templates/hammerspoon.lua
+++ templates/hammerspoon.lua
@@ -1,7 +1,26 @@
+local autoHideEnabled = true
+
+-- Create a set of terminal apps we want to monitor
+local terminalApps = {
+	["Ghostty"] = true,
+	["kitty"] = true,
+}
+
 -- Hide all apps after terminal is selected
 hs.window.filter.default:subscribe(hs.window.filter.windowFocused, function(window, appName)
-	-- hs.notify.new({title="Hammerspoon", informativeText=appName}):send()
-	if appName == "kitty" then
+	-- hs.notify.new({ title = "Hammerspoon", informativeText = appName }):send()
+	if terminalApps[appName] and autoHideEnabled then
 		hs.eventtap.keyStroke({ "cmd", "option" }, "h")
 	end
+end)
+
+hs.hotkey.bind({ "cmd", "option" }, "t", function()
+	autoHideEnabled = not autoHideEnabled
+	-- Optional: Show notification about the state change
+	hs.notify
+		.new({
+			title = "Hammerspoon",
+			informativeText = autoHideEnabled and "Terminal auto-hide enabled" or "Terminal auto-hide disabled",
+		})
+		:send()
 end)
