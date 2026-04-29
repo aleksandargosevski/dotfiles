ca73ed7ac6b02b5db4a077dc14c4e3aec7af5ddf
Aleksandar Goševski <gosevskibg@gmail.com>
2026-04-29 09:10:13 +0200

    feat: add theme toggle

--- nvim/lua/goschevski/plugins/catppuccin.lua
+++ nvim/lua/goschevski/plugins/catppuccin.lua
@@ -1,5 +1,17 @@
-local theme = "macchiato"
--- local theme = "latte"
+-- Read theme mode from ~/.theme (dark/light), default to dark (mocha)
+local function get_flavour()
+	local f = io.open(os.getenv("HOME") .. "/.theme", "r")
+	if f then
+		local mode = f:read("*l")
+		f:close()
+		if mode == "light" then
+			return "latte"
+		end
+	end
+	return "mocha"
+end
+
+local theme = get_flavour()
 
 return {
 	"catppuccin/nvim",
